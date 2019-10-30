3635ec082046d3bb0a5467a00772064e4f14bbef
Tamar Auber <tamarauber@Tamars-MBP.cable.rcn.com>
2019-10-30 17:48:46 -0400

    Added comments

--- scripts.js
+++ scripts.js
@@ -1,139 +1,157 @@
-$( document ).ready(function() {
-  
-
-//Global variables
-
-let firstNumber = ""; //sets to empty string
-let secondNumber = ""; //sets to empty string
-let result = 0; //sets to zero
-let operator = "";
-let isOperatorClicked = false;  //When we start, we want to make sure Operator is not clicked.
-let isEqualPushed = false;
-const firstnumshow = $("#first-number-printout");
-const secondnumshow = $("#second-number-printout");
-const resultsshow=  $("#result-printout");
-const equalshow=  $("#show-equals");
-
-const operatorshow = $("#operator-printout");
-const operatorbutton = $( ".operator" );
-const equalsbutton = $( ".equals" );
-const numbersbutton = $(".number");
+$(document).ready(function() {
 
 
-/*This function clears and resets everything. It is called at the bottom to initialize the calculator */
-function clearAll() {
+  //Global variables
 
-    firstNumber = ""; //sets to empty string
-    secondNumber = ""; //sets to empty string
-    result = 0; //sets to zero
-    isOperatorClicked = false;
-    isEqualPushed = false;
-   
 
-   firstnumshow.empty();
-   secondnumshow.empty();
-   resultsshow.empty();
-   operatorshow.empty();
+ /*These variables are here to simplify the code */
+  const firstnumshow = $("#first-number-printout");
+  const secondnumshow = $("#second-number-printout");
+  const resultsshow = $("#result-printout");
+  const equalshow = $("#show-equals");
 
-   equalshow.hide();
+  const operatorshow = $("#operator-printout");
+  const operatorbutton = $(".operator");
+  const equalsbutton = $(".equals");
+  const numbersbutton = $(".number");
 
-   equalsbutton.attr("disabled", true);
-   operatorbutton.attr("disabled", false);
-   numbersbutton.attr("disabled", false);
-  
+  let firstNumber = ""; //sets to empty string
+  let secondNumber = ""; //sets to empty string
+  let result = 0; //sets to zero
+  let operator = ""; //sets to empty string
+  let isOperatorClicked = false; //When we start, we want to make sure Operator is not clicked.
 
-}
 
-/*Creates an on click event for the numbers */
+  /*This function clears and resets everything. It is called at the bottom to initialize the calculator */
+  function clearAll() {
 
-numbersbutton.click(function() {
-    console.log("number clicked")
-    if(isOperatorClicked) {
-        secondNumber = secondNumber + $(this).val();
-        secondnumshow.text(secondNumber)
-        equalsbutton.attr("disabled", false);
-    } else {
-        firstNumber = firstNumber + $(this).val();
-        firstnumshow.text(firstNumber)
-    }
-  });
-
-/*Creates an on click event for the operator */
-  operatorbutton.click(function() {
-    isOperatorClicked = true;
-    operatorbutton.attr("disabled", true);
-    operator = $(this).val();
-    
-    switch(operator) {
-        case "add":
-          operatorshow.text("+");
-          break;
-        case "subtract":
-          operatorshow.text("-");
-          break;
-          case "multiply":
-            operatorshow.text("x");
-            break;
-          case "divide":
-            operatorshow.text("/");
-            break;
-            case "power":
-                operatorshow.text("^");
-                break;
-        default:
-          operatorshow.empty();
-      }
-  
-  });
-
-/*Creates an on click event for the equals */
-  equalsbutton.click(function() {
+      firstNumber = ""; //sets to empty string
+      secondNumber = ""; //sets to empty string
+      result = 0; //sets to zero
+      isOperatorClicked = false; //this is the default
      
-   equalshow.show();
-   numbersbutton.attr("disabled", true);
-    equalsbutton.attr("disabled", true);
-    if (isOperatorClicked) {
-        firstNumber = parseInt(firstNumber);
-        secondNumber = parseInt(secondNumber);
-        switch(operator) {
-            case "add":
-              result = firstNumber + secondNumber;
-              resultsshow.text(result);
+
+     //This clears out anything that has been printed to the screen
+      firstnumshow.empty();
+      secondnumshow.empty();
+      resultsshow.empty();
+      operatorshow.empty();
+
+      //We don't want to see the equal sign until we push the button. So, we can hide it.
+
+      equalshow.hide();
+
+      //We want the equals button to be disabled until a second Number is entered. 
+
+      equalsbutton.attr("disabled", true);
+      operatorbutton.attr("disabled", false);
+      numbersbutton.attr("disabled", false);
+
+
+  }
+
+  /*Creates an on click event for the numbers */
+
+  numbersbutton.click(function() {
+      //This first tests if the Operator has been clicked.
+      if (isOperatorClicked) { //If true, we add to the second number
+          secondNumber = secondNumber + $(this).val();
+          secondnumshow.text(secondNumber)
+          //sign there is now a second number, the equals button should be enabled
+          equalsbutton.attr("disabled", false);
+      } else { //if not true, we add to the first number
+          firstNumber = firstNumber + $(this).val();
+          firstnumshow.text(firstNumber)
+      }
+  });
+
+  /*Creates an on click event for the operator */
+  operatorbutton.click(function() {
+    //When the operator is clicked, we want to set the isOperatorClicked to true so we can get to the second button
+      isOperatorClicked = true;
+      //We also want to disable the operator buttons so they can't be pushed again
+      operatorbutton.attr("disabled", true);
+
+      //This sets the value of the operator button to the value of the button pushed
+      operator = $(this).val();
+     //A switch statement is a great way to not have to write else if's here.
+     //In this case, I want the actual operator, not the name to print on the screen
+      switch (operator) {
+          case "add":
+             //This is printing it to the span. It will show up on the sccreen. 
+              operatorshow.text("+");
+            //break is an important part of a switch statement that says "stop here if the condition is met"
               break;
-            case "subtract":
-                result = firstNumber - secondNumber;
-                resultsshow.text(result);
+          case "subtract":
+              operatorshow.text("-");
               break;
+          case "multiply":
+              operatorshow.text("x");
+              break;
+          case "divide":
+              operatorshow.text("/");
+              break;
+          case "power":
+              operatorshow.text("^");
+              break;
+          default: //If none of the conditions are met (not actually possible, we are going to print nothing)
+              operatorshow.empty();
+      }
+
+  });
+
+  /*Creates an on click event for the equals */
+  equalsbutton.click(function() {
+      //When the equals button is pushed, we want to show the span that holds the equal sign.
+      equalshow.show();
+      //We also want to make sure that the numbers can't be pushed again, so we disable the numbers nad equals button.
+      numbersbutton.attr("disabled", true);
+      equalsbutton.attr("disabled", true);
+
+      //This is a failsafe. Only if an operator is clicked and there is at least one number in the second number should it give results
+      if (isOperatorClicked && secondNumber != "") {
+
+        //Our numbers are a string. parseInt makes them integers.
+          firstNumber = parseInt(firstNumber);
+          secondNumber = parseInt(secondNumber);
+
+          //Another switch statement. Again, this could also be an if/else statement.
+          switch (operator) {
+              case "add":
+                  result = firstNumber + secondNumber;
+                  //This is what prints the results on the screen
+                  resultsshow.text(result);
+                  break;
+              case "subtract":
+                  result = firstNumber - secondNumber;
+                  resultsshow.text(result);
+                  break;
               case "multiply":
-                result = firstNumber * secondNumber;
-                resultsshow.text(result);
-                break;
+                  result = firstNumber * secondNumber;
+                  resultsshow.text(result);
+                  break;
               case "divide":
-                result = firstNumber/ secondNumber;
-                resultsshow.text(result);
-                break;
-                case "power":
-                    result = Math.pow(firstNumber, secondNumber)
-                    resultsshow.text(result);
-                    break;
-            default:
-              resultshow.empty();
+                  result = firstNumber / secondNumber;
+                  resultsshow.text(result);
+                  break;
+              case "power":
+                  result = Math.pow(firstNumber, secondNumber)
+                  resultsshow.text(result);
+                  break;
+              default:
+                  resultshow.empty();
           }
-    }
+      }
   });
 
-/*Creates an on click event for the clear */
-$( ".clear" ).click(function() {
-    clearAll();
-   
-
-   
-    console.log("clear pushed");
- 
+  /*Creates an on click event for the clear */
+  //Since we used clearAll to initialize, we can just call it here to clear everything out. 
+  $(".clear").click(function() {
+      clearAll();
   });
 
 
 
 
-clearAll();
-});//This is for the document ready. It has to be last. 
+  clearAll();
+}); //This is for the document ready. It has to be last. 
