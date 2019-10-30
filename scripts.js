c04b8445a7f0e96a873056e494ea6a4e8c642d01
Tamar Auber <tamarauber@Tamars-MBP.cable.rcn.com>
2019-10-30 17:04:48 -0400

    Basic working calculator

--- scripts.js
+++ scripts.js
@@ -6,31 +6,46 @@ $( document ).ready(function() {
 let firstNumber = ""; //sets to empty string
 let secondNumber = ""; //sets to empty string
 let result = 0; //sets to zero
+let operator = "";
 let isOperatorClicked = false;  //When we start, we want to make sure Operator is not clicked.
-let firstnumshow = $("#first-number-printout");
-let secondnumshow = $("#second-number-printout");
-let resultsshow=  $("#result-printout");
-let operatorshow = $("#operator-printout");
+let isEqualPushed = false;
+const firstnumshow = $("#first-number-printout");
+const secondnumshow = $("#second-number-printout");
+const resultsshow=  $("#result-printout");
+const equalshow=  $("#show-equals");
+
+const operatorshow = $("#operator-printout");
+const operatorbutton = $( ".operator" );
+const equalsbutton = $( ".equals" );
+
 
 /*This function clears and resets everything. It is called at the bottom to initialize the calculator */
 function clearAll() {
 
     firstNumber = ""; //sets to empty string
     secondNumber = ""; //sets to empty string
-     result = 0; //sets to zero
+    result = 0; //sets to zero
     isOperatorClicked = false;
-    $(".operator").attr("disabled", false);
+    isEqualPushed = false;
+   
 
    firstnumshow.empty();
    secondnumshow.empty();
    resultsshow.empty();
    operatorshow.empty();
 
+   equalshow.hide();
+
+   equalsbutton.attr("disabled", false);
+   operatorbutton.attr("disabled", false);
+  
+
 }
 
 /*Creates an on click event for the numbers */
 
 $( ".number" ).click(function() {
+    console.log("number clicked")
     if(isOperatorClicked) {
         secondNumber = secondNumber + $(this).val();
         secondnumshow.text(secondNumber)
@@ -41,10 +56,10 @@ $( ".number" ).click(function() {
   });
 
 /*Creates an on click event for the operator */
-  $( ".operator" ).click(function() {
+  operatorbutton.click(function() {
     isOperatorClicked = true;
-    $(".operator").attr("disabled", true);
-    let operator = $(this).val();
+    operatorbutton.attr("disabled", true);
+    operator = $(this).val();
     
     switch(operator) {
         case "add":
@@ -65,17 +80,52 @@ $( ".number" ).click(function() {
         default:
           operatorshow.empty();
       }
-
+  
   });
 
 /*Creates an on click event for the equals */
-  $( ".equals" ).click(function() {
-    $("#show-equals").toggleClass("hidden");
+  equalsbutton.click(function() {
+     
+   equalshow.show();
+    equalsbutton.attr("disabled", true);
+    if (isOperatorClicked) {
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+        switch(operator) {
+            case "add":
+              result = firstNumber + secondNumber;
+              resultsshow.text(result);
+              break;
+            case "subtract":
+                result = firstNumber - secondNumber;
+                resultsshow.text(result);
+              break;
+              case "multiply":
+                result = firstNumber * secondNumber;
+                resultsshow.text(result);
+                break;
+              case "divide":
+                result = firstNumber/ secondNumber;
+                resultsshow.text(result);
+                break;
+                case "power":
+                    result = Math.pow(firstNumber, secondNumber)
+                    resultsshow.text(result);
+                    break;
+            default:
+              resultshow.empty();
+          }
+    }
   });
 
 /*Creates an on click event for the clear */
 $( ".clear" ).click(function() {
-    console.log("clear pushed")
+    clearAll();
+   
+
+   
+    console.log("clear pushed");
+ 
   });
 
 
